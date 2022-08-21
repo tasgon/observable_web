4a8f6b34b10843639f5477c3cc8eed434ca6d18e
tasgon <tasgon_@outlook.com>
2022-08-21 04:32:45 -0400

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import Chunks from './lib/Chunks.svelte';
+  import Individual from './lib/Individual.svelte';
+
+  const hash = window.location.hash.substring(1);
+  let tabs = [
+    {name: "Individual Results", comp: Individual},
+    {name: "Chunks", comp: Chunks}
+  ]
+  let activeTab = tabs[0].comp;
+</script>
+
+<main>
+  <nav class="navbar">
+    {#each tabs as { name, comp }}
+      <button on:click={e => activeTab = comp} class={activeTab === comp ? "active" : ""}>{name}</button>
+    {/each}
+  </nav>
+
+  <svelte:component this={activeTab} />
+</main>
+
+<style>
+  .active {
+    text-shadow: -0.03ex 0 currentColor, 0.06ex 0 currentColor;
+  }
+  .navbar {
+    width: 100vw;
+    border-bottom: 1px gray;
+  }
+</style>
