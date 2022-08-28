34f63b719e00175dd80f52bede3204594ab76f8b
tasgon <tasgon_@outlook.com>
2022-08-28 01:02:35 -0400

    minor page & workflow tweaks

--- frontend/src/lib/Home.svelte
+++ frontend/src/lib/Home.svelte
@@ -2,8 +2,6 @@
   import { data_map, notify } from "../data";
   import type { Profile } from "../types";
 
-  import logo from "../assets/logo.png";
-
   let files: FileList | null = null;
 
   function handle_file(file: File | null | undefined) {
@@ -41,15 +39,13 @@
   }}
   class={"dropper " + (drop_count > 0 ? "on" : "")}>
   <div class="center">
-    <img src={logo} alt="Observable logo" class="logo" />
+    <img src="/logo.png" alt="Observable logo" class="logo" />
     <h1 style="margin-top: 1rem;">Observable</h1>
     <h3>a thing by <a href="https://tas.sh">tas</a></h3>
   </div>
-
-  <div class="top">
-    <div class="stuff">
-      <!-- prettier-ignore -->
-      <p>
+  <div class="stuff">
+    <!-- prettier-ignore -->
+    <p>
     Observable profiles (tile) entities and shows you what's taking up tick time
     and where. Forge and Fabric compatible.
     Get it on
@@ -57,29 +53,28 @@
     <a href="https://modrinth.com/mod/observable">Modrinth</a>, or
     <a href="https://github.com/tasgon/observable/releases">GitHub</a>.
   </p>
-      <p>
-        Wanna figure out how to use it? Go
-        <a href="https://github.com/tasgon/observable/wiki">here</a> for an
-        incomplete wiki, or ask me on
-        <a href="https://discord.gg/sfPbb3b5tF">Discord</a> for help.
-      </p>
+    <p>
+      Wanna figure out how to use it? Go
+      <a href="https://github.com/tasgon/observable/wiki">here</a> for an
+      incomplete wiki or ask me on
+      <a href="https://discord.gg/sfPbb3b5tF">Discord</a> for help.
+    </p>
 
-      <form>
-        <p>
-          Have a local profile you want to analyze? Drop it anywhere on the
-          screen, or just click
-          <label for="file-upload" style="cursor: pointer; color: #646cff;"
-            >here</label
-          >.
-        </p>
-        <input
-          type="file"
-          id="file-upload"
-          style="display: none;"
-          bind:files
-          accept=".json" />
-      </form>
-    </div>
+    <form>
+      <p>
+        Have a local profile you want to analyze? Drop it anywhere on the
+        screen, or just click
+        <label for="file-upload" style="cursor: pointer; color: #646cff;"
+          >here</label
+        >.
+      </p>
+      <input
+        type="file"
+        id="file-upload"
+        style="display: none;"
+        bind:files
+        accept=".json" />
+    </form>
   </div>
 </div>
 
@@ -100,15 +95,9 @@
     text-align: center;
   }
 
-  .top {
-    display: flex;
-    flex-direction: column;
-    height: 100%;
-  }
-
   .stuff {
-    display: table;
     margin: auto;
+    padding: 0 1em;
     flex: 1;
     max-width: 42em;
     font-size: larger;
