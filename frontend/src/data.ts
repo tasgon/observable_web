9410db986e056a8e3819a05c12f676172520aefe
tasgon <tasgon_@outlook.com>
2022-10-23 00:51:18 -0700

    put dim in quotes

--- frontend/src/data.ts
+++ frontend/src/data.ts
@@ -53,7 +53,7 @@ export function get_tp_command(
   dimension: string,
   entry: number | Position
 ): string {
-  let cmd = `/observable tp ${dimension} `;
+  let cmd = `/observable tp \"${dimension}\" `;
   if (typeof entry === "number") {
     cmd += `entity ${entry}`;
   } else {
