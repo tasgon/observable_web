de07d11cf0916badb0955ae5cec0ede576824351
tasgon <me@tas.sh>
2023-09-02 15:23:41 -0700

    move to top-level, small visual tweaks

---
+++ src/routes/profile/[id]/+layout.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import type { LayoutData } from './$types';
+	import { Clock, Hourglass, User } from 'lucide-svelte';
+
+	export let data: LayoutData;
+
+	$: basePath = `/profile/${$page.params.id}`;
+	$: tabs = [
+		{ name: 'Individual', path: `${basePath}` },
+		{ name: 'Chunks', path: `${basePath}/chunks` },
+		{ name: 'Aggregate', path: `${basePath}/aggregated` },
+		{ name: 'Info', path: `${basePath}/info` }
+	];
+
+	$: info = data.diagnostics;
+
+	$: user = 'unknown';
+	$: if (info.user) {
+		fetch(`https://api.ashcon.app/mojang/v2/user/${info.user}`)
+			.then((res) => res.json())
+			.then((res) => (user = res.username));
+	}
+	$: duration = info?.duration ? Math.floor(info.duration / 1000.0).toString() : null;
+</script>
+
+<nav class="mt-0 pl-2 flex flex-row items-end w-full bg-slate-800">
+	{#each tabs as { name, path }}
+		{@const focused = $page.url.pathname === path}
+		<a
+			href={path}
+			class="p-2 mx-1 rounded-t-md {focused ? 'bg-slate-900' : 'bg-slate-700'}"
+			class:hover:bg-slate-800={!focused}
+		>
+			{name}
+		</a>
+	{/each}
+
+	<div class="flex-row ml-8 hidden sm:flex mb-2">
+    	<User />
+		{user}
+		{#if info.start}
+			{@const date_str = new Date(info.start).toLocaleString()}
+			<Clock class="ml-2" />
+			{date_str}
+		{/if}
+		{#if duration}
+			<Hourglass class="ml-2" />
+			{duration}s
+		{/if}
+	</div>
+</nav>
+
+<div class="mx-2 pt-1">
+	<slot />
+</div>
---
+++ src/routes/v1/add/+server.ts
@@ -0,0 +1,5 @@
+export async function POST({ request }) {
+  const body = request.body;
+
+  return new Response("");
+}
