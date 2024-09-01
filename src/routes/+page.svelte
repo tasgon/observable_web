d569c7f7f3725a0afffa44c85fdf66aaf1ac9ddf
tasgon <me@tas.sh>
2024-09-01 10:20:35 -0700

    a very minimal faq

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -32,29 +32,46 @@
     <p>a thing by <a href="https://tas.sh">tas</a></p>
   </div>
   <div class="max-w-xl pt-1 gap-3 [&>*]:my-2">
+    <h1>What is this?</h1>
     <p>
-      Observable profiles (tile) entities and shows you what's taking up tick time and where. Forge
-      and Fabric compatible. Get it on
+      Observable profiles (tile) entities and shows you what's taking up tick time and where. It's available for Minecraft 1.16 - 1.20
+      (with early builds for 1.21 available <a href="https://github.com/tasgon/observable/actions?query=branch%3A1.21">here</a>)
+      on (Neo)Forge and Fabric. Get it on
       <a href="https://www.curseforge.com/minecraft/mc-mods/observable">CurseForge</a>,
       <a href="https://modrinth.com/mod/observable">Modrinth</a>, or
       <a href="https://github.com/tasgon/observable/releases">GitHub</a>.
     </p>
+
+    <h1>How do I use it?</h1>
+    <div>
+      <p>
+        First, make sure you have permissions to run it. By default, only server operators
+        can use the tool. They can allow everyone to use the tool by running:
+      </p>
+      <ul class="list-disc ml-6 mt-1">
+        <li><code>/observable set allPlayersAllowed true</code> to allow everyone, or</li>
+        <li><code>/observable allow &lt;username&gt;</code> to allow a specific user.</li>
+      </ul>
+    </div>
+      
     <p>
-      To get started, bind the "Show profiler screen" key to whatever you prefer, or use
-      <code>/observable</code>.
+      Once that's taken care of, bind the "Show profiler screen" key to whatever
+      you prefer, or run <code>/observable run &lt;duration&gt;</code> if you're an operator.
     </p>
+
+    <h1>I'm still confused!</h1>
     <p>
-      For more details, go
-      <a href="https://github.com/tasgon/observable/wiki">here</a> for an incomplete wiki or ask me
-      on
-      <a href="https://discord.gg/sfPbb3b5tF">Discord</a> for help.
+      I have an incomplete wiki available <a href="https://github.com/tasgon/observable/wiki">here</a>
+      or you can talk to me on
+      <a href="https://discord.gg/sfPbb3b5tF">Discord</a>.
     </p>
 
     <form>
-      <p>
-        Have a local profile you want to analyze? Open it
-        <label for="file-upload" style="cursor: pointer; color: #646cff;">here</label>.
-      </p>
+      <label for="file-upload" class="cursor-pointer rounded-md bg-slate-800 w-full py-2 flex justify-center">
+        <p class="text-blue-400 text-lg">
+          Have a local profile you want to analyze? Open it here.
+        </p>
+      </label>
       <input type="file" id="file-upload" style="display: none;" bind:files accept=".json" />
     </form>
   </div>
@@ -63,5 +80,9 @@
 <style lang="postcss">
   a {
     @apply text-blue-400;
+  }
+
+  h1 {
+    @apply text-2xl font-semibold;
   }
 </style>
